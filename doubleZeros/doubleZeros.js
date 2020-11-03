115dfd06fc9aeea1634e05a087f725066b31d778
josilob <bjosilo@gmail.com>
2020-11-03 13:30:28 -0500

    another solution for dblZero

--- doubleZeros/doubleZeros.js
+++ doubleZeros/doubleZeros.js
@@ -18,3 +18,19 @@ const doubleZeros = (arr) => {
 };
 
 doubleZeros([1, 0, 2, 3, 0, 4, 5, 0]);
+
+//// or
+
+const dblZeros = function (arr) {
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] === 0) {
+			arr.splice(i, 0, 0);
+			i++;
+			arr.pop();
+		}
+	}
+	return arr;
+};
+testArr = [1, 0, 2, 0, 1, 4, 5, 1];
+
+console.log(dblZeros(testArr));
