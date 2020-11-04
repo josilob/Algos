56eea4249ae6fbacb128545708bee0f95d2a509b
josilob <bjosilo@gmail.com>
2020-11-04 13:56:22 -0500

    two additional solutions for unique

--- Ruby/unique_elements/unique.rb
+++ Ruby/unique_elements/unique.rb
@@ -13,3 +13,27 @@ def unique_letters(ary)
   print ary
 end
 unique_letters(letters_arr)
+
+def unique_letters2 (ary)
+  unique=[]
+  for letter in 0...ary.length do
+    unless unique.index(ary[letter])
+      unique.push ary[letter]
+    end
+  end
+  return unique
+end
+unique_letters2(letters_arr)
+
+
+def unique_letters3(ary)
+  new_array = []
+  ary.each do |letter|
+    if new_array.include? letter
+    else 
+      new_array.push(letter)
+    end
+  end
+  p new_array
+end
+unique_letters3(letters_arr)
