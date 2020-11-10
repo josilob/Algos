84c88e6ab4bb72fd7af15749e141a1627982fb77
josilob <bjosilo@gmail.com>
2020-11-09 23:52:00 -0500

    additional solutions DNA_Ruby

--- Ruby/DNA_Ruby_2DArr/dna.rb
+++ Ruby/DNA_Ruby_2DArr/dna.rb
@@ -13,3 +13,41 @@ end
 p dna_string('ATTG')
 
 
+def dna_string2(string)
+  new_dna = []
+  string.split('').map do |i|
+    case (i) 
+  when 'C'
+      new_dna.push(['C', 'G']);
+  when 'G'
+      new_dna.push(['G', 'C']);
+  when 'A'
+      new_dna.push(['A', 'T']);  
+  when 'T'
+      new_dna.push(['T', 'A']);
+    end
+  end
+  return new_dna
+end
+
+p dna_string2('ATGC')
+
+
+def dna_string3(str)
+    map = {
+        A: "T",
+        T: "A",
+        C: "G",
+        G: "C"
+    }
+    comps = []
+
+    0.upto(str.length - 1) do |i|
+        char = str[i].to_sym
+        comps.push([char.to_s, map[char]])
+    end
+
+    return comps
+end
+
+p dna_string3("ATGC")
