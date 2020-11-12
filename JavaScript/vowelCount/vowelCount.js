ac4daecc2cc647eaf4007c59a1c84c0a5d4610a3
josilob <bjosilo@gmail.com>
2020-11-11 23:30:39 -0500

    vowel count

--- JavaScript/vowelCount/vowelCount.js
+++ JavaScript/vowelCount/vowelCount.js
@@ -8,3 +8,23 @@
 6. If character is a vowel then increment total by 1
 7. After loop ends, return total, which is the vowel count
 */
+
+function vowelCount(str) {
+	let total = 0;
+	str = str.toLowerCase();
+	for (let i = 0; i < str.length; i++) {
+		let char = str[i];
+		if (
+			char === 'a' ||
+			char === 'e' ||
+			char === 'i' ||
+			char === 'o' ||
+			char === 'u'
+		) {
+			total += 1;
+		}
+	}
+	console.log(str);
+	return total;
+}
+console.log(vowelCount('All cows eat grass'));
