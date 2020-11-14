1a6a6449e7ba3c4b6b4dd0b3bcd1e4973323f9bb
josilob <bjosilo@gmail.com>
2020-11-13 23:17:50 -0500

    anagram.rb

--- Ruby/anagram/anagram.rb
+++ Ruby/anagram/anagram.rb
@@ -1,21 +1,34 @@
-# Anagram
-
-Write a method `anagrams?` that takes in two words and returns a boolean indicating whether or not the words are anagrams. Anagrams are words that contain the same characters but not necessarily in the same order. Solve this without using `.sort`
-
-```ruby
-  # @param string
+# @param string
   # @param string
   # @return boolean
 
-def anagrams?(word1, word2)
-  #Happy hacking!
+def anagrams? (word1, word2)
+
+  if word1.length != word2.length
+    return false
+  end
+
+  char_list = word2.split('')
+  position1 = 0
+  valid = true
+
+  while position1 < word1.length && valid
+    position2 = 0
+    found = false
+
+    while position2 < char_list.length && !found
+      if word1[position1] == char_list[position2]
+        found = true
+      else position2 += 1
+      end
+    end
+
+    if found
+      char_list[position2] = nil
+    else
+      valid = false
+    end
+    position1 += 1
+  end
+  return valid
 end
-
-puts anagrams?("cat", "act")          #=> true
-puts anagrams?("restful", "fluster")  #=> true
-puts anagrams?("cat", "dog")          #=> false
-puts anagrams?("boot", "bootcamp")    #=> false
-```
-### Deliverables 
-
-Add your names onto [this google sheet](https://docs.google.com/spreadsheets/d/13tuilM86zSqZxsdBO4Ee5SLX22qjEL1x1xY0oljYbOY/edit#gid=1683573624) first person in the room is the driver, second person is the navagator 
