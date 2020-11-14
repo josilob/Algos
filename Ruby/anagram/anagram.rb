54c137e95240467c9c0d47addcf9b16a86928136
josilob <bjosilo@gmail.com>
2020-11-13 23:15:56 -0500

    anagram algo

---
+++ Ruby/anagram/anagram.rb
@@ -0,0 +1,21 @@
+# Anagram
+
+Write a method `anagrams?` that takes in two words and returns a boolean indicating whether or not the words are anagrams. Anagrams are words that contain the same characters but not necessarily in the same order. Solve this without using `.sort`
+
+```ruby
+  # @param string
+  # @param string
+  # @return boolean
+
+def anagrams?(word1, word2)
+  #Happy hacking!
+end
+
+puts anagrams?("cat", "act")          #=> true
+puts anagrams?("restful", "fluster")  #=> true
+puts anagrams?("cat", "dog")          #=> false
+puts anagrams?("boot", "bootcamp")    #=> false
+```
+### Deliverables 
+
+Add your names onto [this google sheet](https://docs.google.com/spreadsheets/d/13tuilM86zSqZxsdBO4Ee5SLX22qjEL1x1xY0oljYbOY/edit#gid=1683573624) first person in the room is the driver, second person is the navagator 
