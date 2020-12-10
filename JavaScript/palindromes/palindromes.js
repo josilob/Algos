b3607160a0174fd201e4cccf3ff37f476813e82f
josilob <bjosilo@gmail.com>
2020-12-10 16:14:13 -0500

    .every solution

--- JavaScript/palindromes/palindromes.js
+++ JavaScript/palindromes/palindromes.js
@@ -6,7 +6,10 @@ function palindrome(str) {
 // console.log(palindrome('abba'));
 // console.log(palindrome('Abba')); // hard check (===) gives false
 
-
 function palindrome2(str) {
-    
+	return str.split('').every((char, i) => {
+		return char === str[str.length - i - 1];
+	});
 }
+// console.log(palindrome2('watermelon'));
+// console.log(palindrome2('stressed desserts'));
