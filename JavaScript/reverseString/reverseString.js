bddd320b7f753f44beea457f6f470ae92b07d60e
josilob <bjosilo@gmail.com>
2020-12-09 22:48:08 -0500

    multiple reverse ways

--- JavaScript/reverseString/reverseString.js
+++ JavaScript/reverseString/reverseString.js
@@ -1,6 +1,33 @@
 function reverse(str) {
-	const arr = str.split('');
-	arr.reverse();
-	return arr.join('');
+	return str.split('').reverse().join('');
 }
-console.log(reverse('I am reversing a string'))
+// console.log(reverse('I am reversing a string'));
+
+// ################  forEach  ############### //
+
+function reverse2(str) {
+	const chars = str.split('');
+	let reversed = [];
+	chars.forEach((char) => reversed.unshift(char));
+	return reversed.join('');
+}
+// console.log(reverse2('How to reverse a string'));
+
+// ############### for...of ################### //
+function reverse3(str) {
+	let reversed = '';
+	for (let char of str) {
+		reversed = char + reversed;
+	}
+	return reversed;
+}
+// console.log(reverse3('How to reverse a string'));
+
+// ################# REDUCE  ################### //
+function reverse4(str) {
+	return str
+		.split('')
+		.reduce((reversed, character) => character + reversed, '');
+	return reversed;
+}
+// console.log(reverse4('How to reverse a string'));
