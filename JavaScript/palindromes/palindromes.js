04e4212fe44a6e7f831be116d9ad5bbca61af084
josilob <bjosilo@gmail.com>
2020-12-10 15:56:15 -0500

    initial solution

---
+++ JavaScript/palindromes/palindromes.js
@@ -0,0 +1,12 @@
+function palindrome(str) {
+	return str === str.split('').reverse().join('') ? true : false;
+}
+
+//console.log(palindrome('apple'));
+// console.log(palindrome('abba'));
+// console.log(palindrome('Abba')); // hard check (===) gives false
+
+
+function palindrome2(str) {
+    
+}
