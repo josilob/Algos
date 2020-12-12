421ace6f0f1738718b9b46ea3e5233abcb375955
josilob <bjosilo@gmail.com>
2020-12-11 23:13:21 -0500

    refactor, explanation

--- JavaScript/reverseInt/reverseInt.js
+++ JavaScript/reverseInt/reverseInt.js
@@ -1,5 +1,13 @@
 function reverseInt(n) {
+	const reversedNum = parseInt(n.toString().split('').reverse().join(''));
+	// if number < 0, multiply with -1 or Math.sign(number)
+	return reversedNum * Math.sign(n);
+}
+// ONE-LINER
+function reverseInt2(n) {
 	return parseInt(n.toString().split('').reverse().join('')) * Math.sign(n);
 }
 
-console.log(reverseInt(-9510));
+console.log(reverseInt(951));
+console.log(reverseInt(-951000));
+console.log(reverseInt(-9501000));
