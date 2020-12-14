9a277aab4922eb37c0f5e8b6c09757878cf74c55
josilob <bjosilo@gmail.com>
2020-12-14 18:27:50 -0500

    chunk2

---
+++ JavaScript/arrayChunk/chunk.js
@@ -0,0 +1,21 @@
+function chunk(arr, size) {
+	return Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+		arr.slice(i * size, i * size + size)
+	);
+}
+// console.log(chunk([1, 2, 3, 4, 5, 6, 7], 3));
+
+function chunk2(arr, size) {
+	const chunked = [];
+	for (let element of arr) {
+		const last = chunked[chunked.length - 1];
+
+		if (!last || last.length === size) {
+			chunked.push([element]);
+		} else {
+			last.push(element);
+		}
+	}
+	return chunked;
+}
+console.log(chunk2([1, 2, 3, 4, 5, 6, 7], 3));
