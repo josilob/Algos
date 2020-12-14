a58588b8a14b95eff365e97b0fac9b7e83fa63d4
josilob <bjosilo@gmail.com>
2020-12-14 18:43:35 -0500

    slice chunk solution

--- JavaScript/arrayChunk/chunk.js
+++ JavaScript/arrayChunk/chunk.js
@@ -18,4 +18,15 @@ function chunk2(arr, size) {
 	}
 	return chunked;
 }
-console.log(chunk2([1, 2, 3, 4, 5, 6, 7], 3));
+// console.log(chunk2([1, 2, 3, 4, 5, 6, 7], 3));
+
+function chunk3(arr, size) {
+	const chunked = [];
+	let index = 0;
+	while (index < arr.length) {
+		chunked.push(arr.slice(index, index + size));
+		index += size;
+	}
+	return chunked;
+}
+// console.log(chunk3([1, 2, 3, 4, 5, 6, 7], 3));
