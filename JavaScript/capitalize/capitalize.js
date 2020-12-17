a55ab2e6382a3ef37b38d971cbafea3c5b586289
josilob <bjosilo@gmail.com>
2020-12-16 22:34:20 -0500

    two way capitalize

--- JavaScript/capitalize/capitalize.js
+++ JavaScript/capitalize/capitalize.js
@@ -4,4 +4,18 @@ function capitalize(str) {
 		.map((word) => word[0].toUpperCase() + word.slice(1));
 	return upCaseArr.join(' ');
 }
-// console.log(capitalize('beatufil day outside'));
+// console.log(capitalize('beatuful day outside'));
+
+function capitalize2(str) {
+	let result = str[0].toUpperCase();
+
+	for (let i = 1; i < str.length; i++) {
+		if (str[i - 1] === ' ') {
+			result += str[i].toUpperCase();
+		} else {
+			result += str[i];
+		}
+	}
+	return result;
+}
+console.log(capitalize2('beatuful day outside'));
