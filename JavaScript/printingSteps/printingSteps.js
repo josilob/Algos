4a979d2a4d296286c2daee26214529b25ba9d36c
josilob <bjosilo@gmail.com>
2020-12-17 16:02:11 -0500

    quadratic for loop solution

---
+++ JavaScript/printingSteps/printingSteps.js
@@ -0,0 +1,16 @@
+const steps = (n) => {
+	for (let i = 0; i < n; i++) {
+		let stair = '';
+
+		for (let j = 0; j < n; j++) {
+			if (j <= i) {
+				stair += '#';
+			} else {
+				stair += ' ';
+			}
+		}
+		console.log(stair);
+	}
+};
+
+steps(5)
