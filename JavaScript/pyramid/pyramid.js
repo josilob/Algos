717a247f2b34dca2ac59431a6bbfcc816749f42d
josilob <bjosilo@gmail.com>
2020-12-21 14:09:27 -0500

    pyramid recursively

--- JavaScript/pyramid/pyramid.js
+++ JavaScript/pyramid/pyramid.js
@@ -14,5 +14,25 @@ function pyramid(n) {
 		console.log(level);
 	}
 }
+//pyramid(8);
 
-pyramid(8);
+// Recursive solution
+
+function pyramid2(n, row = 0, level = '') {
+	if (row === n) return;
+
+	if (level.length === 2 * n - 1) {
+		console.log(level);
+		return pyramid(n, row + 1);
+	}
+
+	const midpoint = Math.floor((2 * n - 1) / 2);
+	let add;
+	if (midpoint - row <= level.length && midpoint + row >= level.length) {
+		add = '#';
+	} else {
+		add = ' ';
+	}
+	pyramid(n, row, level + add);
+}
+pyramid2(9);
--- JavaScript/pyramid/readme.md
+++ JavaScript/pyramid/readme.md
@@ -3,3 +3,17 @@
 Write a function that accepts the positive number N.
 The function should console log the pyrmid shape with N levels using the # character.
 Make sure the pyramid has the spaces on both left and the right sides.
+
+```js
+pyramid(8) =>
+
+//        #
+//       ###
+//      #####
+//     #######
+//    #########
+//   ###########
+//  #############
+// ###############
+
+```
