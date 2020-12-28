74cb8ec0bc691138b2e0c9ca5e58c479aad5ec8b
josilob <bjosilo@gmail.com>
2020-12-28 13:03:03 -0500

    vowel count in a string

--- JavaScript/pyramid/pyramid.js
+++ JavaScript/pyramid/pyramid.js
@@ -20,13 +20,13 @@ function pyramid(n) {
 
 function pyramid2(n, row = 0, level = '') {
 	if (row === n) return;
+	const midpoint = Math.floor((2 * n - 1) / 2);
 
 	if (level.length === 2 * n - 1) {
 		console.log(level);
 		return pyramid(n, row + 1);
 	}
 
-	const midpoint = Math.floor((2 * n - 1) / 2);
 	let add;
 	if (midpoint - row <= level.length && midpoint + row >= level.length) {
 		add = '#';
