99b42882aab3e57d8478b709cfba71b3e928f2a1
josilob <bjosilo@gmail.com>
2021-01-26 22:09:33 -0500

    tribonacci recursively

--- JavaScript/stepsToZero/script.js
+++ JavaScript/stepsToZero/script.js
@@ -1,25 +1,15 @@
-// Given a non-negative integer num, return the number of steps to reduce it to zero.
-// If the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
-// Recursively!!!
+// Tribonacci
+// T(n) = T(n-3) + T(n-2) + T(n-1)
 
-let numberOfSteps = (num) => {
-	let counter = 0;
-	if (num <= 0) {
-		return counter;
-    }
+// The Tribonacci sequence Tn is defined as follows:
+// T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+// Given n, return the value of Tn.
+// RECURSIVELY:
+function tribonacci(num){
+    if (num===0) return 0
+    if (num===1) return 1
+    if (num===2) return 1
+    return tribonacci(num-3) + tribonacci(num-2) + tribonacci(num-1)
+}
 
-    //helper function to keep track of counter
-	const helper = (num, counter) => {
-		if (num === 0) {
-			return counter;
-		}
-		if (num % 2 === 0) {
-			return helper(num / 2, counter + 1);
-		} else {
-			return helper(num - 1, counter + 1);
-		}
-		return counter;
-	};
-	return helper(num, counter);
-};
-console.log(numberOfSteps(14));
+console.log(tribonacci(10))
