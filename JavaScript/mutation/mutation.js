e8cb4ab3f67d5d174abddabbb5dc557a042d950a
josilob <bjosilo@gmail.com>
2021-02-28 23:29:31 -0500

    mutations solved

--- JavaScript/mutation/mutation.js
+++ JavaScript/mutation/mutation.js
@@ -2,4 +2,16 @@
 // For example ['hello', 'Hole'] => true // case insensitive
 // arguments ['hello', 'hey'] => false as there us no 'y' in 'hello'
 
+function mutation(arr) {
+	const [str1, str2] = arr.map((s) => s.toLowerCase());
 
+	for (let i = 0; i < str2.length; i++) {
+		const letter = str2[i];
+		if (str1.indexOf(letter) === -1) {
+			return false;
+		}
+	}
+	return true;
+}
+
+console.log(mutation(['hello', 'hell']));
