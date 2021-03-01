788a2c142300a08a435c3c878ec0ed58a06b6642
josilob <bjosilo@gmail.com>
2021-02-28 23:34:30 -0500

    done

--- JavaScript/falsyValues/falsyValues.js
+++ JavaScript/falsyValues/falsyValues.js
@@ -1,2 +1,8 @@
 // Remove all falsy values from an array
 // Falsy values in JS are false, null, 0, "",undefined and NaN.
+
+function falsey(arr) {
+	return arr.filter((element) => !!element);
+}
+
+console.log(falsey([7, 'ate', '', false, 9]));
