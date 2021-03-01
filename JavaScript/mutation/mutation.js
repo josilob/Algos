33214d46ae8f2020e2edd6dad607c866129b22c9
josilob <bjosilo@gmail.com>
2021-02-28 23:29:19 -0500

    mutations algo

---
+++ JavaScript/mutation/mutation.js
@@ -0,0 +1,5 @@
+// Return true if the string in the first element of the array contains all the letters of the string in the second element of the array.
+// For example ['hello', 'Hole'] => true // case insensitive
+// arguments ['hello', 'hey'] => false as there us no 'y' in 'hello'
+
+
