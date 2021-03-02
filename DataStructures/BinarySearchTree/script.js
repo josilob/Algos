0fb18a6d1a6d38d1cca4cd7019eddc8ce92548b2
josilob <bjosilo@gmail.com>
2021-03-02 18:35:32 -0500

    binary search tree

---
+++ DataStructures/BinarySearchTree/script.js
@@ -0,0 +1,7 @@
+// Binary Search Tree
+// Every node can have at most 2 children positioned relative to the parent
+// One node is to the left, the other is to the right of a parent
+// Values and position are tightly connected
+// Left element is less than parent node, while the node to the right is greater than parent
+
+// Parent Node => left_node(value>left.value) && right_node(value<right.value)
