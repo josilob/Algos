2eda494bb41ab67107120f7991cea3752e46129a
josilob <bjosilo@gmail.com>
2021-03-02 19:01:25 -0500

    contains/search method

--- DataStructures/BinarySearchTree/script.js
+++ DataStructures/BinarySearchTree/script.js
@@ -13,6 +13,7 @@
 // ---Directions:
 // 1) Implement the Node class to create a binary search tree. The construtor should initialize values 'data', 'left' and 'right'.
 // 2) Implement the 'insert' method for the Node class. Insert should accept an argument 'data', then create an insert a new node at the appropriate location in the tree.
+// 3) Implement the 'contains' method for the Node class. Contains should accept a 'data' argument and return the Node in the tree with the same value. If the value isn't in the tree return null.
 
 class Node {
 	constructor(data) {
@@ -31,5 +32,17 @@ class Node {
 		} else if (data > this.data) {
 			this.right = new Node(data);
 		}
+	}
+
+	contains(data) {
+		if (this.data === data) {
+			return this;
+		}
+		if (this.data < data && this.right) {
+			return this.right.contains(data);
+		} else if (this.data > data && this.left) {
+			return this.left.contains(data);
+		}
+		return null;
 	}
 }
