47ce99d99bb59a9463ea6dcfdc54a7d8f347418e
josilob <bjosilo@gmail.com>
2021-03-03 23:57:52 -0500

    selection sort

--- JavaScript/sorting/script.js
+++ JavaScript/sorting/script.js
@@ -2,7 +2,7 @@
 
 function bubbleSort(arr) {
 	for (let i = 0; i < arr.length; i++) {
-		for (let j = 0; i < arr.length - i - 1; j++) {
+		for (let j = 0; j < arr.length - i - 1; j++) {
 			if (arr[j] > arr[j + 1]) {
 				const tempVar = arr[j + 1];
 				arr[j + 1] = arr[j];
@@ -10,4 +10,23 @@ function bubbleSort(arr) {
 			}
 		}
 	}
+	return arr;
+}
+
+function selectionSort(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		let indexOfMin = i;
+
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[j] < arr[indexOfMin]) {
+				indexOfMin = j;
+			}
+		}
+		if (indexOfMin !== i) {
+			let lesser = arr[indexOfMin];
+			arr[indexOfMin] = arr[i];
+			arr[i] = lesser;
+		}
+	}
+	return arr;
 }
