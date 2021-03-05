8232a1f3d33cfd72341f754a25d5c5c0c653f900
josilob <bjosilo@gmail.com>
2021-03-04 21:14:47 -0500

    merge sort

--- JavaScript/sorting/script.js
+++ JavaScript/sorting/script.js
@@ -30,3 +30,30 @@ function selectionSort(arr) {
 	}
 	return arr;
 }
+
+function mergeSort(arr) {
+	if (arr.length === 1) {
+		return arr;
+	}
+
+	const center = Math.floor(arr.length / 2);
+	const left = arr.slice(0, center); // center argument is exclusive, so it will be UNTIL that point in the array
+	const right = arr.slice(center);
+
+	mergeSort(left); // divide further recursively until arrays reach length of 1
+	mergeSort(right);
+
+	return merge(mergeSort(left), mergeSort(right)); // returns array with length of 1 because of the first if statement on top (which are the values needed as arguments for merge function)
+}
+
+function merge(left, right) {
+	const results = [];
+	while (left.length && right.length) {
+		if (left[0] < right[0]) {
+			results.push(left.shift());
+		} else {
+			results.push(right.shift());
+		}
+	}
+	return [...results, ...left, ...right];
+}
