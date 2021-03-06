730c0e7765672a4be467eb5cbbbb60ad140adc01
josilob <bjosilo@gmail.com>
2021-03-05 23:37:43 -0500

    same frequency solution

--- JavaScript/sameFrequency/script.js
+++ JavaScript/sameFrequency/script.js
@@ -4,3 +4,29 @@
 // sameFrequency(182,281) => true
 // sameFrequency(22,222) => false
 // sameFrequency(3589578,5879835) => true
+
+function sameFrequency(num1, num2) {
+	let strNum1 = num1.toString();
+	let strNum2 = num2.toString();
+	if (strNum1.length !== strNum2.length) return false;
+
+	let countNum1 = {};
+	let countNum2 = {};
+
+	for (let i = 0; i < strNum1.length; i++) {
+		countNum1[strNum1[i]] = (countNum1[strNum1[i]] || 0) + 1;
+	}
+
+	for (let j = 0; j < strNum1.length; j++) {
+		countNum2[strNum2[j]] = (countNum2[strNum2[j]] || 0) + 1;
+	}
+
+	for (let key in countNum1) {
+		if (countNum1[key] !== countNum2[key]) return false;
+	}
+
+	return true;
+}
+
+// console.log(sameFrequency(3589578, 58798352));
+// console.log(sameFrequency(3589578, 5879835));
