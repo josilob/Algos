2bc18b55ceb6a4eed56e36c54cdcd58ec010dbdf
josilob <bjosilo@gmail.com>
2021-03-05 23:35:26 -0500

    frequency algo

---
+++ JavaScript/sameFrequency/script.js
@@ -0,0 +1,6 @@
+// Write a function called sameFrequency. Given two positive integers, find out if the two numbers have the same frequency of digits. Your solution must have following complexities: O(n)
+
+//Example:
+// sameFrequency(182,281) => true
+// sameFrequency(22,222) => false
+// sameFrequency(3589578,5879835) => true
