750b0d1db08e6a1ec7caa64113b8dab5442184f7
josilob <bjosilo@gmail.com>
2021-03-08 22:31:15 -0500

    regex solution

--- JavaScript/charCount/script.js
+++ JavaScript/charCount/script.js
@@ -1,1 +1,15 @@
 // Write a function that takes in a string and returns alphanumeric character count!
+
+function charCount(str) {
+	let obj = {};
+	for (let char of str) {
+		char = char.toLowerCase();
+		console.log(char);
+		if (/[a-z0-9]/.test(char)) {
+			obj[char] = ++obj[char] || 1;
+		}
+	}
+	return obj;
+}
+
+console.log(charCount('is ThIs$# Thing WorkING'));
