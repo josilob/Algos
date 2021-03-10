2e57da81808d609b269aecb86fc54ad9ada29331
josilob <bjosilo@gmail.com>
2021-03-10 18:55:21 -0500

    naive solution

--- JavaScript/ProblemSolvingPatterns/multiplePointers/script.js
+++ JavaScript/ProblemSolvingPatterns/multiplePointers/script.js
@@ -2,3 +2,15 @@
 // The function should find the first pair where the sum is 0.
 // Return an array that includes both values that sum to zero or undefined if pair doesn't exist.
 
+// N^2 solution:
+function sumZero(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[i] + arr[j] === 0) {
+				return [arr[i], arr[j]];
+			}
+		}
+	}
+}
+
+// console.log(sumZero([-3, -2, 1, 5, 7, 8, 3, 2]));
