c1a9fca80f4be4b1c3fe27557b3931469e96a935
josilob <bjosilo@gmail.com>
2021-03-10 00:06:10 -0500

    frequency counter improve

--- JavaScript/frequencyCounter/script.js
+++ JavaScript/frequencyCounter/script.js
@@ -5,7 +5,7 @@ function sameFrequency(arr1, arr2) {
 		return false;
 	}
 	for (let i = 0; i < arr1.length; i++) {
-		let correctIndex = arr2.indexOf(arr1[i] ** 2);
+		let correctIndex = arr2.indexOf(arr1[i] ** 2); // 2nd iteration over elements
 		if (correctIndex === -1) {
 			return false;
 		}
@@ -14,3 +14,30 @@ function sameFrequency(arr1, arr2) {
 	return true;
 }
 // console.log(sameFrequency([1, 2, 3, 3, 2, 6], [1, 4, 36, 9, 9, 4]));
+
+//
+// frequency counter pattern
+function sameFrequency2(arr1, arr2) {
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+	let frequencyCounter1 = {};
+	let frequencyCounter2 = {};
+	// two sequenced loops, but not nested make it O(n)
+	for (let val of arr1) {
+		frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+	}
+	for (let val of arr2) {
+		frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+	}
+	for (let key in frequencyCounter1) {
+		if (!(key ** 2 in frequencyCounter2)) {
+			return false;
+		}
+		if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+			return false;
+		}
+	}
+	return true;
+}
+console.log(sameFrequency2([1, 2, 3, 3, 2, 6], [1, 4, 36, 9, 9, 4]));
