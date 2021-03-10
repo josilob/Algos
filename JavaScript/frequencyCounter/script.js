2b256ba39dd73f5313229c32c1fbdb54bc1971c9
josilob <bjosilo@gmail.com>
2021-03-09 23:59:36 -0500

    for loop solution quadr

--- JavaScript/frequencyCounter/script.js
+++ JavaScript/frequencyCounter/script.js
@@ -1,1 +1,16 @@
 // Write a function called sameFrequency that takes in two arrays and returns true if every value in the array one has it's corresponding value squared in the second array. The frequency of values must be the same.
+
+function sameFrequency(arr1, arr2) {
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+	for (let i = 0; i < arr1.length; i++) {
+		let correctIndex = arr2.indexOf(arr1[i] ** 2);
+		if (correctIndex === -1) {
+			return false;
+		}
+		arr2.splice(correctIndex, 1);
+	}
+	return true;
+}
+// console.log(sameFrequency([1, 2, 3, 3, 2, 6], [1, 4, 36, 9, 9, 4]));
