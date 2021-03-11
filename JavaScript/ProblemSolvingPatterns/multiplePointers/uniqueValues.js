fdb9ffbed2ac7b6491c3b546b17dfcf3bca6f814
josilob <bjosilo@gmail.com>
2021-03-10 19:28:59 -0500

    unique values - two pointers

---
+++ JavaScript/ProblemSolvingPatterns/multiplePointers/uniqueValues.js
@@ -0,0 +1,4 @@
+// Implement a function called uniqueValues which accepts a sorted array and counts unique values in the array.
+// There can be negative numbers in the array, but it will always be sorted.
+
+function uniqueValues(arr) {}
