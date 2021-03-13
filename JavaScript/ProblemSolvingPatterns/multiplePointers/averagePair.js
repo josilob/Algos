48c16e59ef2b8e28310bbdbbbbf002b0d06c3cde
josilob <bjosilo@gmail.com>
2021-03-12 23:53:42 -0500

    average pair solution

--- JavaScript/ProblemSolvingPatterns/multiplePointers/averagePair.js
+++ JavaScript/ProblemSolvingPatterns/multiplePointers/averagePair.js
@@ -2,3 +2,17 @@
 
 // Time: O(N), Space O(1)
 
+function averagePair(arr, num) {
+	let start = 0;
+	let end = arr.length - 1;
+	while (start < end) {
+		let avg = (arr[start] + arr[end]) / 2;
+		if (avg === num) return true;
+		else if (avg < num) start++;
+		else end--;
+	}
+	return false;
+}
+
+console.log(averagePair([2, 4, 5, 7, 9], 5)); // false
+console.log(averagePair([1, 4, 9], 5)); // true (9+1)/2
