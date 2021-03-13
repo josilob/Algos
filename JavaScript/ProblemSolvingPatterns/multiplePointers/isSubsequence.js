398b07187c91dd6bbfa7184851c2d8a441311a15
josilob <bjosilo@gmail.com>
2021-03-12 23:56:47 -0500

    recursive solution

--- JavaScript/ProblemSolvingPatterns/multiplePointers/isSubsequence.js
+++ JavaScript/ProblemSolvingPatterns/multiplePointers/isSubsequence.js
@@ -12,3 +12,11 @@ function isSubsequence(str1, str2) {
 	}
 	return false;
 }
+
+//recursive
+function isSubsequence2(str1, str2) {
+	if (str1.length === 0) return true;
+	if (str2.length === 0) return false;
+	if (str2[0] === str1[0]) return isSubsequence2(str1.slice(1), str2.slice(1));
+	return isSubsequence2(str1, str2.slice(1));
+}
