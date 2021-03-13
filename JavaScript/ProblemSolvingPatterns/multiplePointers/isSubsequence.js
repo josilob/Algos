5b88d1414fa998c66d5459ee73c723b99fb73d65
josilob <bjosilo@gmail.com>
2021-03-12 23:56:19 -0500

    iterative solution

--- JavaScript/ProblemSolvingPatterns/multiplePointers/isSubsequence.js
+++ JavaScript/ProblemSolvingPatterns/multiplePointers/isSubsequence.js
@@ -1,1 +1,14 @@
 // Write a function called isSubsequence that takes in two strings and checks whether the characters in the first string form a subsequence of the characters in the second string. In other words, the function should check if characters from the first string appear anywhere in the second string, WITHOUT THEIR ORDER CHANGING.
+
+// iterations
+function isSubsequence(str1, str2) {
+	var i = 0;
+	var j = 0;
+	if (!str1) return true;
+	while (j < str2.length) {
+		if (str2[j] === str1[i]) i++;
+		if (i === str1.length) return true;
+		j++;
+	}
+	return false;
+}
