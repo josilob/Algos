420c83444bd40268f86bfdce2a92e2f2aa341470
josilob <bjosilo@gmail.com>
2021-03-16 23:56:22 -0400

    mergeSort implementation

--- JavaScript/Sorting/MergeSort/script.js
+++ JavaScript/Sorting/MergeSort/script.js
@@ -2,7 +2,7 @@
 // Keep splitting unsorted array until they are all size of 1 element.
 // Repeatedly merge them back together while producing new sorted sublist until there is only one array.
 
-// Pseudocode:
+// Merging Sorted Arrays Pseudocode:
 // 1) Create an empty array and take a look at the smallest values in each input array.
 // 2) While there are still values we haven't looked at:
 //   a)if the value in the first array is smaller than the value in the second array, push the value from the first array into results array and move on to the next value in the first array.
@@ -37,7 +37,9 @@ function merge(arr1, arr2) {
 
 // shorter syntax:
 function merge2(arr1, arr2) {
-	let i = 0, j = 0, results = [];
+	let i = 0,
+		j = 0,
+		results = [];
 	while (i < arr1.length && j < arr2.length) {
 		arr1[i] < arr2[j]
 			? (results.push(arr1[i]), ++i)
@@ -49,3 +51,15 @@ function merge2(arr1, arr2) {
 	return results;
 }
 
+// Merge Sort Pseudocode:
+// 1) Break up array into halves until you have arrays that are empty or have one element
+// 2) Once you have smaller sorted arrays, merge those arrays with other sorted arrays until you are back at the full length of the array
+// 3) Once the array has been merged back together, return the merged (and sorted!) array
+
+function mergeSort(arr) {
+	if (arr.length <= 1) return arr;
+	let midpoint = Math.floor(arr.length / 2);
+	let left = mergeSort(arr.slice(0, mid));
+	let right = mergeSort(arr.slice(mid));
+	return merge(left, right);
+}
