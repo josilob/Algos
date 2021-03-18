64ccbd22fe790f2c6eb89da726c064210fdc9810
josilob <bjosilo@gmail.com>
2021-03-17 23:45:18 -0400

    solution;

--- JavaScript/Algos/differentNumber/script.js
+++ JavaScript/Algos/differentNumber/script.js
@@ -6,3 +6,20 @@
 // iqTest("2 4 7 8 10") => 3 // Third number is odd, while the rest of the numbers are even
 
 // iqTest("1 2 1 1") => 2 // Second number is even, while the rest of
+
+function iqTest(numbers) {
+	numbers = numbers.split(' ');
+
+	let positions = [[], []];
+
+	for (let i = 0; i < numbers.length; i++) {
+		positions[numbers[i] % 2].push(i + 1);
+	}
+
+	if (positions[0].length === 1) return positions[0][0];
+	if (positions[1].length === 1) return positions[1][0];
+
+	return 0;
+}
+
+console.log(iqTest('1 3 5 7 7 9 11 6')); // => 8th(last) element of string
