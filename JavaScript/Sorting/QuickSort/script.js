3e45af28d15adfd8566cdc2e3092326547563036
josilob <bjosilo@gmail.com>
2021-03-17 23:23:51 -0400

    full review

--- JavaScript/Sorting/QuickSort/script.js
+++ JavaScript/Sorting/QuickSort/script.js
@@ -52,3 +52,24 @@ function pivot(arr, start = 0, end = arr.length + 1) {
 // Call the pivot helper on the array
 // When the helper returns the updated pivot idx, recursively call the pivot helper on the subarray to the left of that idx, and the subarray to the right of that idx
 
+function quickSort(arr, left = 0, right = arr.length - 1) {
+	if (left < right) {
+		let pivotIndex = pivot(arr, left, right);
+		// left
+		quickSort(arr, left, pivotIndex - 1);
+		// right
+		quickSort(arr, pivotIndex + 1, right);
+	}
+	return arr;
+}
+
+// console.log(quickSort([1, 6, 2, 3, 7, 4, 9]));
+
+// BigO of QuickSort
+
+// Time Complexity:
+//    -Best Case: O(NlogN)
+//    -Average: O(NlogN)
+//    -Worst: O(n^2)
+
+// Space Complexity: O(logN)
