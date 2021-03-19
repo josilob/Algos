2bba0d2f383e3d990e0f499838ff694be6daacc2
josilob <bjosilo@gmail.com>
2021-03-18 23:44:39 -0400

    radix sort written methods

--- JavaScript/Sorting/RadixSort/script.js
+++ JavaScript/Sorting/RadixSort/script.js
@@ -13,3 +13,25 @@
 // getDigit(7323,1) //=>2
 // getDigit(7323,3) //=>7
 
+function getDigit(num, i) {
+	return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+}
+
+// digitCount(num) // returns the number of digits in num
+// digitCount(25); =>2 // digitCount(314); =>3
+function digitCount(num) {
+	if (num === 0) return 1;
+	// 10 to what power gives us the number (log at base of 10)
+	return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+// Math.log10(423) => 2.6263...
+// Math.abs(2.6263) => 2 ... 2+1 = 3
+
+// mostDigits([1234,56,7]) // => 4
+function mostDigits(nums) {
+	let maxDigits = 0;
+	for (let i = 0; i < nums.length; i++) {
+		maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+	}
+	return maxDigits;
+}
