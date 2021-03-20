e36d80c00a2e9c1f90162384c270440c0ca27cf1
josilob <bjosilo@gmail.com>
2021-03-20 13:40:25 -0400

    notes

--- JavaScript/Algos/stringIncrementer/script.js
+++ JavaScript/Algos/stringIncrementer/script.js
@@ -10,9 +10,12 @@
 // Attention: If the number has leading zeros the amount of digits should be considered.
 
 function incrementString(string) {
+	// if string doesn't end with a number, add number 1 to the end of it
 	if (isNaN(string.slice(-1))) {
 		return `${string}1`;
 	} else {
+		// extract characters (isNaN(n)) => filters characters that are not a number
+		// isNaN(n) recognizes numbers inside quotes too //=> isNaN('55') is false just as if it was isNaN(55)
 		let chars = string
 			.split('')
 			.filter((n) => isNaN(n))
@@ -21,7 +24,10 @@ function incrementString(string) {
 			.split('')
 			.filter((n) => !isNaN(n))
 			.join('');
-		let incrementedNum = +nums + 1;
+		let incrementedNum = +nums + 1; // +nums => converts operand to a number (workaround)
+		// so if string was ending with a number, this is a 'sneaky' way to convert it to the integer in this case so it can be incremented (as opposed to the string where it would be just appended)
+
+		// edge case with numbers that are 'zero padded' //=> 099 > 100
 		if (nums.length > incrementedNum.toString().length) {
 			let zeros = '';
 			for (let i = 0; i < nums.length; i++) {
