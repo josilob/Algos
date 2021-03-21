fc89ed2ab6c98a54a343f23c64e727641d542662
josilob <bjosilo@gmail.com>
2021-03-21 14:32:47 -0400

    simplified solution;

--- JavaScript/Algos/calculatingWithFunctions/script.js
+++ JavaScript/Algos/calculatingWithFunctions/script.js
@@ -13,59 +13,101 @@
 // Division should be integer division. For example, this should return 2, not 2.666666...:
 // eight(dividedBy(three()));
 
-function expression(number, operation) {
-	if (!operation) return number;
-	return operation(number);
+let num = function (digit) {
+	return function (operation) {
+		return operation ? operation(digit) : digit;
+	};
+};
+let zero = num(0);
+let one = num(1);
+let two = num(2);
+let three = num(3);
+let four = num(4);
+let five = num(5);
+let six = num(6);
+let seven = num(7);
+let eight = num(8);
+let nine = num(9);
+
+function plus(r) {
+	return function (l) {
+		return l + r;
+	};
+}
+function minus(r) {
+	return function (l) {
+		return l - r;
+	};
+}
+function times(r) {
+	return function (l) {
+		return l * r;
+	};
+}
+function dividedBy(r) {
+	return function (l) {
+		return Math.floor(l / r);
+	};
 }
 
-function zero(operation) {
-	return expression(0, operation);
-}
-function one(operation) {
-	return expression(1, operation);
-}
-function two(operation) {
-	return expression(2, operation);
-}
-function three(operation) {
-	return expression(3, operation);
-}
-function four(operation) {
-	return expression(4, operation);
-}
-function five(operation) {
-	return expression(5, operation);
-}
-function six(operation) {
-	return expression(6, operation);
-}
-function seven(operation) {
-	return expression(7, operation);
-}
-function eight(operation) {
-	return expression(8, operation);
-}
-function nine(operation) {
-	return expression(9, operation);
-}
+console.log(five(times(six())));
+console.log(nine(dividedBy(four())));
 
-function plus(x) {
-	return function (y) {
-		return y + x;
-	};
-}
-function minus(x) {
-	return function (y) {
-		return y - x;
-	};
-}
-function times(x) {
-	return function (y) {
-		return y * x;
-	};
-}
-function dividedBy(x) {
-	return function (y) {
-		return Math.floor(y / x);
-	};
-}
+// ANOTHER SOLUTION
+
+// function expression(number, operation) {
+// 	if (!operation) return number;
+// 	return operation(number);
+// }
+
+// function zero(operation) {
+// 	return expression(0, operation);
+// }
+// function one(operation) {
+// 	return expression(1, operation);
+// }
+// function two(operation) {
+// 	return expression(2, operation);
+// }
+// function three(operation) {
+// 	return expression(3, operation);
+// }
+// function four(operation) {
+// 	return expression(4, operation);
+// }
+// function five(operation) {
+// 	return expression(5, operation);
+// }
+// function six(operation) {
+// 	return expression(6, operation);
+// }
+// function seven(operation) {
+// 	return expression(7, operation);
+// }
+// function eight(operation) {
+// 	return expression(8, operation);
+// }
+// function nine(operation) {
+// 	return expression(9, operation);
+// }
+
+// function plus(x) {
+// 	return function (y) {
+// 		return y + x;
+// 	};
+// }
+// function minus(x) {
+// 	return function (y) {
+// 		return y - x;
+// 	};
+// }
+// function times(x) {
+// 	return function (y) {
+// 		return y * x;
+// 	};
+// }
+// function dividedBy(x) {
+// 	return function (y) {
+// 		return Math.floor(y / x);
+// 	};
+// }
