75619612a13abe314e781f4797cb38ed27a41a9c
josilob <bjosilo@gmail.com>
2021-03-21 14:36:45 -0400

    last added solution (analyze for later)

--- JavaScript/Algos/calculatingWithFunctions/script.js
+++ JavaScript/Algos/calculatingWithFunctions/script.js
@@ -111,3 +111,29 @@ console.log(nine(dividedBy(four())));
 // 		return Math.floor(y / x);
 // 	};
 // }
+
+// For later : analyze this solution:
+
+// [ 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', ]
+// .forEach(function (name, n) { this[name] = function (f) { return f ? f(n) : n; }; });
+
+// function plus(n) {
+// 	return function (a) {
+// 		return a + n;
+// 	};
+// }
+// function minus(n) {
+// 	return function (a) {
+// 		return a - n;
+// 	};
+// }
+// function times(n) {
+// 	return function (a) {
+// 		return a * n;
+// 	};
+// }
+// function dividedBy(n) {
+// 	return function (a) {
+// 		return a / n;
+// 	};
+// }
