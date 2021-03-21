aa87a53f4a8e81fee2d9e936eb8c10352097260f
josilob <bjosilo@gmail.com>
2021-03-21 14:26:35 -0400

    calculations

--- JavaScript/Algos/calculatingWithFunctions/script.js
+++ JavaScript/Algos/calculatingWithFunctions/script.js
@@ -12,3 +12,60 @@
 // The most outer function represents the left operand, the most inner function represents the right operand
 // Division should be integer division. For example, this should return 2, not 2.666666...:
 // eight(dividedBy(three()));
+
+function expression(number, operation) {
+	if (!operation) return number;
+	return operation(number);
+}
+
+function zero(operation) {
+	return expression(0, operation);
+}
+function one(operation) {
+	return expression(1, operation);
+}
+function two(operation) {
+	return expression(2, operation);
+}
+function three(operation) {
+	return expression(3, operation);
+}
+function four(operation) {
+	return expression(4, operation);
+}
+function five(operation) {
+	return expression(5, operation);
+}
+function six(operation) {
+	return expression(6, operation);
+}
+function seven(operation) {
+	return expression(7, operation);
+}
+function eight(operation) {
+	return expression(8, operation);
+}
+function nine(operation) {
+	return expression(9, operation);
+}
+
+function plus(x) {
+	return function (y) {
+		return y + x;
+	};
+}
+function minus(x) {
+	return function (y) {
+		return y - x;
+	};
+}
+function times(x) {
+	return function (y) {
+		return y * x;
+	};
+}
+function dividedBy(x) {
+	return function (y) {
+		return Math.floor(y / x);
+	};
+}
