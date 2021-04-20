1422f7c4fd1a734b1f012d3ee994aef23e171609
josilob <bjosilo@gmail.com>
2021-04-20 23:58:25 +0200

    domain solution

--- JavaScript/Algos/domainName/script.js
+++ JavaScript/Algos/domainName/script.js
@@ -3,3 +3,25 @@
 // domainName("http://github.com/carbonfive/raygun") == "github"
 // domainName("http://www.zombie-bites.com") == "zombie-bites"
 // domainName("https://www.cnet.com") == "cnet"
+
+
+function domainName(url) {
+	let patt = /(\/{2}|\.)/g;
+	let result = url.match(patt);
+	let sliced = '';
+	//console.log(result);
+	if (result[0] == '//') {
+		if (url.charAt(url.indexOf(result[0]) + 2) == 'w') {
+			sliced = url.slice(
+				url.indexOf(result[1]) + 1,
+				url.indexOf(result[1], url.indexOf(result[1]) + 1)
+			);
+			console.log(sliced);
+		} else {
+			sliced = url.slice(url.indexOf(result[0]) + 2, url.indexOf(result[1]));
+		}
+	} else {
+		sliced = url.slice(url.indexOf(result[0]) + 1, url.lastIndexOf(result[1]));
+	}
+	return sliced;
+}
