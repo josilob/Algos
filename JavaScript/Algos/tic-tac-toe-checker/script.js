785f79931ef0be18d3e554fa2819f17697a846bf
josilob <bjosilo@gmail.com>
2021-04-23 23:57:01 +0200

    initial try

--- JavaScript/Algos/tic-tac-toe-checker/script.js
+++ JavaScript/Algos/tic-tac-toe-checker/script.js
@@ -12,3 +12,39 @@
 // 2 if "O" won,
 // 0 if it's a cat's game (i.e. a draw).
 // You may assume that the board passed in is valid in the context of a game of Tic-Tac-Toe.
+
+function isSolved(board) {
+	const getWins = makeGetWins(board);
+	return getWins(1) ? 1 : getWins(2) ? 2 : emptySlots(board) ? -1 : 0;
+}
+
+const makeGetWins = (board) => (key) =>
+	[
+		horizontalWin(board, key),
+		verticalWin(board, key),
+		leftDiagWin(board, key),
+		rightDiagWin(board, key),
+	].some((_) => Boolean(_));
+
+const emptySlots = (board) =>
+	board.map((x) => x.some((_) => _ === 0)).some((_) => Boolean(_));
+
+const horizontalWin = (board, key) =>
+	board.map((x) => x.every((_) => _ === key)).some((_) => Boolean(_));
+
+const verticalWin = (board, key) => horizontalWin(rotateBoard(board), key);
+
+const leftDiagWin = (board, key) =>
+	[0, 1, 2]
+		.map((winIdx, rowIdx) => board[rowIdx][winIdx] === key)
+		.every((_) => Boolean(_));
+
+const rightDiagWin = (board, key) =>
+	[2, 1, 0]
+		.map((winIdx, rowIdx) => board[rowIdx][winIdx] === key)
+		.every((_) => Boolean(_));
+
+const rotateBoard = (board) =>
+	Array(board[0].length)
+		.fill(null)
+		.map((x, idx) => board.map((_) => _[idx]));
