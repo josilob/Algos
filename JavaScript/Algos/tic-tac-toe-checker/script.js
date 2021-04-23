734ed042e31a871f4ca6f07e2e6139ab34282e83
josilob <bjosilo@gmail.com>
2021-04-23 23:58:16 +0200

    cleaner solution w/ regex

--- JavaScript/Algos/tic-tac-toe-checker/script.js
+++ JavaScript/Algos/tic-tac-toe-checker/script.js
@@ -48,3 +48,13 @@ const rotateBoard = (board) =>
 	Array(board[0].length)
 		.fill(null)
 		.map((x, idx) => board.map((_) => _[idx]));
+
+    /////// # SHORT SOLUTION # ////////
+
+    function isSolved(board) {
+					board = board.join('-').replace(/,/g, '');
+					if (/222|2...2...2|2....2....2|2..2..2/.test(board)) return 2;
+					if (/111|1...1...1|1....1....1|1..1..1/.test(board)) return 1;
+					if (/0/.test(board)) return -1;
+					return 0;
+				}
