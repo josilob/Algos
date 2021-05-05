aef66caa083e502388e053311059e0639e89e134
josilob <bjosilo@gmail.com>
2021-05-06 00:07:59 +0200

    binets formula solution

--- JavaScript/Algos/perimeterOfSquares/script.js
+++ JavaScript/Algos/perimeterOfSquares/script.js
@@ -1,4 +1,3 @@
-
 // Perimeter of squares in a rectangle
 // The drawing shows 6 squares the sides of which have a length of 1, 1, 2, 3, 5, 8. It's easy to see that the sum of the perimeters of these squares is : 4 * (1 + 1 + 2 + 3 + 5 + 8) = 4 * 20 = 80
 
@@ -13,23 +12,28 @@ function perimeter(n) {
 	return 4 * arr.reduce((sum, num) => sum + num, 0);
 }
 
-function perimeter(n) {
-	var total = 0;
-	var first = 1;
-	var second = 1;
-	var third;
+function perimeter2(n) {
+	let total = 0,
+		first = 1,
+		second = 1,
+		third;
+
 	for (let i = 0; i < n + 1; i++) {
 		third = first + second;
 		total += first;
 
-		console.log('first=', first);
-		console.log('second=', second);
-		console.log('third=', third);
-		console.log('total=', total);
-		console.log('------------');
+		// console.log('first=', first);
+		// console.log('second=', second);
+		// console.log('third=', third);
+		// console.log('total=', total);
+		// console.log('------------');
 
 		first = second;
 		second = third;
 	}
 	return total * 4;
 }
+
+// /w Binet's Formula
+const perimeter3 = (n) =>
+	4 * Math.round(((1 + 5 ** 0.5) / 2) ** (n + 3) / 5 ** 0.5 - 1);
