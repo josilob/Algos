917ebde94040121260caf3e01ca850d53cafdc39
josilob <bjosilo@gmail.com>
2021-05-07 23:16:02 +0200

    similar solution with match regex

--- JavaScript/Algos/arabianString/script.js
+++ JavaScript/Algos/arabianString/script.js
@@ -12,3 +12,12 @@ function camelize(str) {
 		(v) => v.slice(0, 1).toUpperCase() + v.slice(1).toLowerCase()
 	).join``;
 }
+
+function camelize2(str) {
+	return str
+		.match(/[a-z0-9]+/gi)
+		.map(function (s) {
+			return s[0].toUpperCase() + s.substr(1).toLowerCase();
+		})
+		.join('');
+}
