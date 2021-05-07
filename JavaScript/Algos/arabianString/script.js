11cb653a8471cca41ef0def63b7c9726f6b411d8
josilob <bjosilo@gmail.com>
2021-05-07 23:11:32 +0200

    arabian str init;

---
+++ JavaScript/Algos/arabianString/script.js
@@ -0,0 +1,6 @@
+// You must create a method that can convert a string from any format into PascalCase. This must support symbols too.
+
+// Don't presume the separators too much or you could be surprised.
+
+// For example: (Input --> Output)
+
