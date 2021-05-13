3719aa3c6a7e28a3d37dc6bcfaa9fab911cf2b1d
josilob <bjosilo@gmail.com>
2021-05-13 17:45:41 -0400

    reduce example

--- JavaScript/Algos/alphabetSymetry/script.js
+++ JavaScript/Algos/alphabetSymetry/script.js
@@ -16,19 +16,20 @@ function solve(arr) {
 		let counter = 0;
 
 		for (let i = 0; i <= word.length - 1; i++) {
-			if (i === word[i].toUpperCase().charCodeAt() - 65) counter++;
+			if (i === word[i].toUpperCase().charCodeAt() - 'A'.charCodeAt()) counter++;
 		}
 		solutionArr.push(counter);
 	});
 	return solutionArr;
 }
 
-// console.log(solve(['abode', 'ABc', 'xyzD'])); //[ 4, 3, 1 ]
+console.log(solve(['abode', 'ABc', 'xyzD'])); //[ 4, 3, 1 ]
 // console.log(solve(['IAMDEFANDJKL', 'thedefgh', 'xyzDEFghijabc'])); //[ 6, 5, 7 ]
 
 // loop through all the words in the array
 // pick a word, loop through its letters
-// check if index of each letter as uppercase matches charCodeAt-65
+// check if index of each letter as uppercase matches charCodeAt()-65 (deducting 65 since uppercase chars start from ascii value 65)
+// if you can't remember the characters code starting position, it is always possible to use 'A'.charCodeAt() which would return 65
 // if it matches, increment the counter for that word
 // at the end of the word, push the counter value in the array
 // return the entire array
@@ -44,9 +45,17 @@ function solve2(arr) {
 	);
 }
 
-// console.log(solve2(['abode', 'ABc', 'xyzD'])); //[ 4, 3, 1 ]
+console.log(solve2(['abode', 'ABc', 'xyzD'])); //[ 4, 3, 1 ]
 
 // declare alphabet string
 // map through array of words from parameter 'arr'
 // check for each word in the array and filter its characters whose idx in a word matches idx in the alphabet string
 // return number of remaining(matching) characters after FILTER for each word, that way their position matches the requirement in the algorithm
+
+// Reduce way
+
+function solve3(arr) {
+	return arr.map((x) =>
+		[...x.toLowerCase()].reduce((s, v, i) => s + (v.charCodeAt() == i + 97), 0)
+	);
+}
