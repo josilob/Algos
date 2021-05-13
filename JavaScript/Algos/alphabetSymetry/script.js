03edbcaf8187cb17e726f27a76889115a8ed12db
josilob <bjosilo@gmail.com>
2021-05-13 17:15:29 -0400

    1st solution

--- JavaScript/Algos/alphabetSymetry/script.js
+++ JavaScript/Algos/alphabetSymetry/script.js
@@ -8,3 +8,27 @@
 // Input will consist of alphabet characters, both uppercase and lowercase. No spaces.
 
 // Good luck!
+
+function solve(arr) {
+	let solutionArr = [];
+
+	arr.forEach((word) => {
+		let counter = 0;
+
+		for (let i = 0; i <= word.length - 1; i++) {
+			if (i === word[i].toUpperCase().charCodeAt() - 65) counter++;
+		}
+		solutionArr.push(counter);
+	});
+	return solutionArr;
+}
+
+console.log(solve(['abode', 'ABc', 'xyzD'])); //[ 4, 3, 1 ]
+console.log(solve(['IAMDEFANDJKL', 'thedefgh', 'xyzDEFghijabc'])); //[ 6, 5, 7 ]
+
+// loop through all the words in the array
+// pick a word, loop through its letters
+// check if index of each letter as uppercase matches charCodeAt-65
+// if it matches, increment the counter for that word
+// at the end of the word, push the counter value in the array
+// return the entire array
