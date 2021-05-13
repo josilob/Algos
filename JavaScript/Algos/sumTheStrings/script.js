190328ba5cfe260d2381604e0c7d182867462fc0
josilob <bjosilo@gmail.com>
2021-05-13 18:32:01 -0400

    solutions

--- JavaScript/Algos/sumTheStrings/script.js
+++ JavaScript/Algos/sumTheStrings/script.js
@@ -4,3 +4,16 @@
 
 // "4",  "5" --> "9"
 // "34", "5" --> "39"
+
+function sumStr(a, b) {
+	return String(Number(a) + Number(b));
+}
+
+// simple conversion
+function sumStr2(a, b) {
+	return +a + +b + '';
+}
+
+function sumStr3(a, b) {
+	return ((parseInt(a) || 0) + (parseInt(b) || 0)).toString();
+}
