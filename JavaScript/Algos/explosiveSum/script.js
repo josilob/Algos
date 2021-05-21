a159f580f8fb3c9e2bbc687006fc1fe8d6728c1d
josilob <bjosilo@gmail.com>
2021-05-21 14:32:47 -0400

    explosive sum w/ memoize

--- JavaScript/Algos/explosiveSum/script.js
+++ JavaScript/Algos/explosiveSum/script.js
@@ -22,3 +22,16 @@
 // sum(80) // 15796476
 // sum(100) // 190569292
 
+let memo = [];
+
+function sum(n, m = n) {
+	if (n == 0) return 1;
+	if (n < 0 || m == 0) return 0;
+	if (memo[n] && memo[n][m]) return memo[n][m];
+	let total = sum(n, m - 1) + sum(n - m, m);
+	if (!memo[n]) {
+		memo[n] = [];
+	}
+	memo[n][m] = total;
+	return total;
+}
