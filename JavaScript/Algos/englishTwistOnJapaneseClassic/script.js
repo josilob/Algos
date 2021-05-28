001515b77ae3130d8a5d50fa11011eaec1e1f2a2
josilob <bjosilo@gmail.com>
2021-05-27 23:37:35 -0400

    additional solutions

--- JavaScript/Algos/englishTwistOnJapaneseClassic/script.js
+++ JavaScript/Algos/englishTwistOnJapaneseClassic/script.js
@@ -45,3 +45,24 @@ function shiritori(words) {
 	}
 	return arr;
 }
+
+
+function shiritori2(words) {
+	let res = [];
+	for (let w of words) {
+		if (!w.length || (res.length && w[0] !== res[res.length - 1].slice(-1)))
+			break;
+		res.push(w);
+	}
+	return res;
+}
+
+
+function shiritori3(words) {
+	if (!words.length || words[0] === '') return [];
+
+	for (let i = 1; i < words.length; i++)
+		if (words[i - 1].slice(-1) !== words[i][0]) return words.slice(0, i);
+
+	return words;
+}
