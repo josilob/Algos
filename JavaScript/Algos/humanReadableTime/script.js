a3c654b58dced928d8f3e46a7523ac9bd6f68463
josilob <bjosilo@gmail.com>
2021-06-08 22:57:57 -0400

    solved

--- JavaScript/Algos/humanReadableTime/script.js
+++ JavaScript/Algos/humanReadableTime/script.js
@@ -6,3 +6,16 @@
 // The maximum time never exceeds 359999 (99:59:59)
 
 // You can find some examples in the test fixtures.
+
+function humanReadable(seconds) {
+	var pad = function (x) {
+		return x < 10 ? '0' + x : x;
+	};
+	return (
+		pad(parseInt(seconds / (60 * 60))) +
+		':' +
+		pad(parseInt((seconds / 60) % 60)) +
+		':' +
+		pad(seconds % 60)
+	);
+}
