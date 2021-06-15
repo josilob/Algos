51e220230fb2de16d5ea4c94efb18bf05942d55e
josilob <bjosilo@gmail.com>
2021-06-14 22:47:31 -0400

    solved

--- JavaScript/Algos/maxMultiple/script.js
+++ JavaScript/Algos/maxMultiple/script.js
@@ -3,4 +3,15 @@
 // - N is less than or equal to bound.
 // - N is greater than 0.
 
-function maxMultiple(divisor, bound) {}
+function maxMultiple(divisor, bound) {
+	return bound - (bound % divisor);
+}
+
+function maxMultiple2(divisor, bound) {
+	while (bound > 0) {
+		if (bound % divisor === 0) return bound;
+	}
+	bound--;
+}
+
+console.log(maxMultiple(5, 25));
