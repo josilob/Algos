19c77d4496c470e8379b1f02066dd23e1fe377cf
josilob <bjosilo@gmail.com>
2021-06-14 22:42:42 -0400

    max multiple init

---
+++ JavaScript/Algos/maxMultiple/script.js
@@ -0,0 +1,6 @@
+// Given a divisor and a bound, find the largest integer N such that:
+// - N is divisible by divisor.
+// - N is less than or equal to bound.
+// - N is greater than 0.
+
+function maxMultiple(divisor, bound) {}
