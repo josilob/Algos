82edcdd826a9ddb99da7a8e23b64e5f9b6a69107
josilob <bjosilo@gmail.com>
2021-06-14 22:49:15 -0400

    extra num

---
+++ JavaScript/Algos/extraNumber/script.js
@@ -0,0 +1,3 @@
+// You're given three integers, a, b and c. It is guaranteed that two of these integers are equal to each other. What is the value of the third integer?
+
+function extraNumber(a, b, c) {}
