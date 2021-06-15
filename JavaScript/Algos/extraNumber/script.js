025e03ee542395b9ddcb2e80e8b42b4aec666948
josilob <bjosilo@gmail.com>
2021-06-14 22:55:57 -0400

    bitwise optional solution

--- JavaScript/Algos/extraNumber/script.js
+++ JavaScript/Algos/extraNumber/script.js
@@ -1,3 +1,15 @@
 // You're given three integers, a, b and c. It is guaranteed that two of these integers are equal to each other. What is the value of the third integer?
 
-function extraNumber(a, b, c) {}
+function extraNumber(a, b, c) {
+	// Bitwise XOR
+	// The bitwise XOR operator (^) returns a 1 in each bit position for which the corresponding bits of either but not both operands are 1s.
+	return a ^ b ^ c;
+}
+
+function extraNumber2(a, b, c) {
+	if (a === b) return c;
+	if (b === c) return a;
+	else return b;
+}
+
+console.log(extraNumber(1, 1));
