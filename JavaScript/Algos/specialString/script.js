5b09ff00a3f61841455d33bc6c6da2ddf83ac22f
josilob <bjosilo@gmail.com>
2021-07-20 23:54:24 -0400

    substring count

---
+++ JavaScript/Algos/specialString/script.js
@@ -0,0 +1,6 @@
+function substrCount(n, s) {
+	s = s.split``;
+}
+
+// abcbaba => 10 (1 for each character + same characters in a row + same characters around different middle char)
+// [a, b, c, b, a, b, a, bcb, bab, aba]
