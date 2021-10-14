04d386c3fca3bd704df76477a075266d89a6c225
Bojan J <bjosilo@gmail.com>
2021-10-13 23:07:24 -0400

    frequence sequence added solution

--- JavaScript/Algos/frequencySequence/script.js
+++ JavaScript/Algos/frequencySequence/script.js
@@ -12,3 +12,18 @@ function freqSeq(str, sep) {
 	}, {});
 	return letters.map((l) => count[l]).join(sep);
 }
+
+function freqSeq2(str, sep) {
+	return str
+		.split('')
+		.map((v, i, arr) => arr.filter((vi) => vi === v).length)
+		.join(sep);
+}
+
+const freqSeq3 = (s, sep) => {
+	s.split('')
+		.map((i) => s.split(i).length - 1)
+		.join(sep);
+};
+
+console.log(freqSeq3('hello there', ' '));
