2a7b4cf757702cb33bc7319af8820be48bb024d7
Bojan J <bjosilo@gmail.com>
2021-11-08 23:59:55 -0500

    solved

--- JavaScript/Algos/mostUsedWord/script.js
+++ JavaScript/Algos/mostUsedWord/script.js
@@ -24,3 +24,39 @@
 // Bonus points (not really, but just for fun):
 // Avoid creating an array whose memory footprint is roughly as big as the input text.
 // Avoid sorting the entire array of unique words.
+
+
+function topThreeWords(text) {
+	let regex = '.*[a-zA-Z].*';
+	if (text.match(regex)) {
+		let wordMap = new Map();
+		text.split(' ').forEach((word) => {
+			if (word) {
+				word = word.toLowerCase();
+				if (wordMap.has(word)) {
+					let count = wordMap.get(word);
+					count++;
+					wordMap.set(word, count);
+				} else {
+					wordMap.set(word, 1);
+				}
+			}
+		});
+		const sortedWordMap = new Map(
+			[...wordMap.entries()].sort((a, b) => b[1] - a[1])
+		);
+
+		let result = Array.from(sortedWordMap.keys()).filter(
+			(word, index) => index < 3
+		);
+		result = result.map((res) => {
+			res = res.replace(/[/.,]/g, '');
+			if (res !== '') {
+				return res;
+			}
+		});
+		return result.filter((res) => res !== undefined);
+	} else {
+		return [];
+	}
+}
