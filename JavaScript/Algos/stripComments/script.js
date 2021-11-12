4a665d7497b6d9a0aae891d90386345837f28699
Bojan J <bjosilo@gmail.com>
2021-11-11 23:55:38 -0500

    trim comments

---
+++ JavaScript/Algos/stripComments/script.js
@@ -0,0 +1,32 @@
+// Complete the solution so that it strips all text that follows any of a set of comment markers passed in. Any whitespace at the end of the line should also be stripped out.
+
+// Example:
+
+// Given an input string of:
+
+// apples, pears # and bananas
+// grapes
+// bananas !apples
+// The output expected would be:
+
+// apples, pears
+// grapes
+// bananas
+// The code would be called like so:
+
+// var result = solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
+// result should == "apples, pears\ngrapes\nbananas"
+
+const solution = (input, markers) => {
+	let arr = input.split('\n');
+
+	markers.forEach((marker) => {
+		arr = arr.map((el) => {
+			const idx = el.indexOf(marker);
+			if (idx !== -1) {
+				return el.substring(0, idx);
+			} else return el.replace(/\s*$/, '');
+		});
+	});
+	return arr.join('\n').trim();
+};
