6e479f7f4dc2e4578eaa4029861c531e7f52365d
Bojan J <bjosilo@gmail.com>
2021-11-24 23:25:11 -0500

    solved matching pairs

--- JavaScript/Algos/sockMerchant/script.js
+++ JavaScript/Algos/sockMerchant/script.js
@@ -6,3 +6,17 @@
 
 // There is one pair of color 1 and one of color 2. There are three odd socks left, one of each color. The number of pairs is 2.
 
+function sockMerchant(n, ar) {
+	let pair = 0;
+	ar.sort();
+	for (let i = 0; i < n; i++) {
+		if (ar[i] == ar[i + 1]) {
+			// increment the pairs by 1 and skip next element's iteration so it doesn't repeat
+			i++;
+			pair++;
+		}
+	}
+	return pair;
+}
+
+console.log(sockMerchant(7, [1, 2, 1, 2, 1, 3, 2])); // => 2 pairs
