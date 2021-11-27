ed40c3f6728a70374301c61241f07cebcef0bf30
Bojan J <bjosilo@gmail.com>
2021-11-26 23:42:12 -0500

    bon apetit solution

--- JavaScript/Algos/bonApetit/script.js
+++ JavaScript/Algos/bonApetit/script.js
@@ -13,3 +13,9 @@
 // b: the amount of money that Anna contributed to the bill
 
 
+function bonAppetit(bill, k, b) {
+	const sum = bill.reduce((r, v) => r + v, 0);
+	const result = (sum - bill[k]) / 2;
+	if (b - result === 0) console.log('Bon Appetit');
+	else console.log(b - result);
+}
