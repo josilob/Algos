4692d1ba08c1883b2a6b6a84f377dc741c142ce1
Bojan J <bjosilo@gmail.com>
2021-11-30 22:41:22 -0500

    easier approach

--- JavaScript/Algos/bouncingBalls/script.js
+++ JavaScript/Algos/bouncingBalls/script.js
@@ -31,3 +31,10 @@ function bouncingBall(h, bounce, window, i = 0) {
 
 	return -1;
 }
+
+function bouncingBall2(h, bounce, window) {
+	var rebounds = -1;
+	if (bounce > 0 && bounce < 1)
+		while (h > window) (rebounds += 2), (h *= bounce);
+	return rebounds;
+}
