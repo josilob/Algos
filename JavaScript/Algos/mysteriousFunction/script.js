890f45b0a5579631df63be3abc7aca4883e05fdd
Bojan J <bjosilo@gmail.com>
2021-12-01 23:59:39 -0500

    first solution

--- JavaScript/Algos/mysteriousFunction/script.js
+++ JavaScript/Algos/mysteriousFunction/script.js
@@ -6,3 +6,16 @@
 // getNum(89282350306) #-> returns 8
 // getNum(3479283469) #-> returns 5
 // The archaeologists were totally stuck with this challenge. They were all in desperation but then.... came YOU the SUPER-AWESOME programmer. Will you be able to understand the mystery of this function and rewrite it?
+
+var getNum = function (n) {
+	let result = 0;
+	n = n.toString().split('');
+	n.forEach((el) =>
+		el == 0 || el == 6 || el == 9
+			? (result += 1)
+			: el == 8
+			? (result += 2)
+			: (result += 0)
+	);
+	return result;
+};
