efef0619602fddcad5276bf17b9ffabd4612c170
Bojan J <bjosilo@gmail.com>
2021-12-03 23:43:07 -0500

    solution with coordinates

--- JavaScript/Algos/tenMinuteWalk/script.js
+++ JavaScript/Algos/tenMinuteWalk/script.js
@@ -12,3 +12,28 @@ function isValidWalk(walk) {
 
 	return true;
 }
+
+function isValidWalk2(walk) {
+	var dx = 0;
+	var dy = 0;
+	var dt = walk.length;
+
+	for (var i = 0; i < walk.length; i++) {
+		switch (walk[i]) {
+			case 'n':
+				dy--;
+				break;
+			case 's':
+				dy++;
+				break;
+			case 'w':
+				dx--;
+				break;
+			case 'e':
+				dx++;
+				break;
+		}
+	}
+
+	return dt === 10 && dx === 0 && dy === 0;
+}
