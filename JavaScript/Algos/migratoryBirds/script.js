727579330bbe0f3dbe80b6507ae5aadb99781485
Bojan J <bjosilo@gmail.com>
2021-12-06 23:55:03 -0500

    solved migratory birds

--- JavaScript/Algos/migratoryBirds/script.js
+++ JavaScript/Algos/migratoryBirds/script.js
@@ -18,3 +18,27 @@
 
 // The first line contains an integer, , the size of .
 // The second line describes  as  space-separated integers, each a type number of the bird sighted.
+
+function migratoryBirds(arr) {
+	let map = {},
+		maxCount = 0,
+		output = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (!map[arr[i]]) {
+			map[arr[i]] = 1;
+		} else {
+			map[arr[i]] += 1;
+		}
+
+		// getting max count
+		if (map[arr[i]] > maxCount) {
+			maxCount = map[arr[i]];
+		}
+	}
+	for (let j = 0; j < arr.length; j++) {
+		if (map[arr[j]] === maxCount) {
+			output.push(arr[j]);
+		}
+	}
+	return Math.min(...output);
+}
