4852fd6098a1c92c416d8f416753b495e38d4c9b
Bojan J <bjosilo@gmail.com>
2021-12-09 23:13:16 -0500

    solved year of programmer

---
+++ JavaScript/Algos/dayOfTheProgrammer/script.js
@@ -0,0 +1,11 @@
+function solve(year) {
+	if (year == 1918) return '26.09.1918';
+
+	if (
+		(year <= 1917 && year % 4 == 0) ||
+		year % 400 == 0 ||
+		(year % 4 == 0) & (year % 100 != 0)
+	)
+		return '12.09.' + year;
+	else return '13.09.' + year;
+}
