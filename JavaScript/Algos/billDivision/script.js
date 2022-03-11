d74604302337cbf4edc783aa347583dc544a0236
Bojan Josilo <bjosilo@gmail.com>
2022-03-11 14:09:47 -0600

    bill division

---
+++ JavaScript/Algos/billDivision/script.js
@@ -0,0 +1,6 @@
+function bonAppetit(bill, k, b) {
+	const sum = bill.reduce((r, v) => r + v, 0);
+	const result = (sum - bill[k]) / 2;
+	if (b - result === 0) console.log('Bon Appetit');
+	else console.log(b - result);
+}
